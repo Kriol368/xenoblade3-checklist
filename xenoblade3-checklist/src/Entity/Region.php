b9883068e3369646be1081f4722419ffd14e9078
Kriol368 <kriol368@gmail.com>
2024-10-21 18:52:18 +0200

    Some entities created

---
+++ xenoblade3-checklist/src/Entity/Region.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\RegionRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: RegionRepository::class)]
+class Region
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $name = null;
+
+    #[ORM\OneToMany(targetEntity: Area::class, mappedBy: 'region')]
+    private Collection $areas;
+
+    public function __construct()
+    {
+        $this->areas = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(?string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Area>
+     */
+    public function getAreas(): Collection
+    {
+        return $this->areas;
+    }
+
+    public function addArea(Area $area): static
+    {
+        if (!$this->areas->contains($area)) {
+            $this->areas->add($area);
+            $area->setRegion($this);
+        }
+
+        return $this;
+    }
+
+    public function removeArea(Area $area): static
+    {
+        if ($this->areas->removeElement($area)) {
+            // set the owning side to null (unless already changed)
+            if ($area->getRegion() === $this) {
+                $area->setRegion(null);
+            }
+        }
+
+        return $this;
+    }
+}
