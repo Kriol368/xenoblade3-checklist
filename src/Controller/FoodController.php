76be06511827e7308130b09ad6dbd382901d2b4a
Kriol368 <kriol368@gmail.com>
2024-10-28 12:57:26 +0100

    trying to implement food

--- src/Controller/FoodController.php
+++ src/Controller/FoodController.php
@@ -1,18 +1,28 @@
 <?php
-
+// src/Controller/FoodController.php
 namespace App\Controller;
 
+use App\Repository\FoodRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class FoodController extends AbstractController
 {
+    private FoodRepository $foodRepository;
+
+    public function __construct(FoodRepository $foodRepository)
+    {
+        $this->foodRepository = $foodRepository;
+    }
+
     #[Route('/food', name: 'app_food')]
     public function index(): Response
     {
+        $foods = $this->foodRepository->findAll();
+
         return $this->render('food/index.html.twig', [
-            'controller_name' => 'FoodController',
+            'foods' => $foods,
         ]);
     }
 }
