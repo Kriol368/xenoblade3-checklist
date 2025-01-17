e45a3d4164323a8d9c15e694e16c5c78a04f07f4
Kriol368 <kriol368@gmail.com>
2025-01-17 09:43:17 +0100

    character card updated

--- public/js/character.js
+++ public/js/character.js
@@ -1,10 +1,12 @@
 $(document).ready(function () {
     const $characterRows = $('.character-row');
-    const $characterCard = $('#character-card');
+    const $card = $('#character-card');
     const $characterName = $('#character-name');
     const $characterClass = $('#character-class');
     const $characterImg = $('#character-img');
-    const $closeCardButton = $('#close-card');
+    const $closeCardBtn = $('#close-card');
+    const $overlay = $("<div class='overlay'></div>").appendTo("body"); // Add overlay dynamically
+
 
     $characterRows.on('click', function () {
         const $row = $(this);
@@ -18,10 +20,19 @@ $(document).ready(function () {
         $characterImg.attr('src', `/img/characters/artwork/strm_hero_book_${index}_01_0.png`);
         $characterImg.attr('alt', name);
 
-        $characterCard.show();
+        $card.show();
+        $overlay.show();
     });
 
-    $closeCardButton.on('click', function () {
-        $characterCard.hide();
+    // Close the card when clicking the close button
+    $closeCardBtn.on("click", function () {
+        $card.hide();
+        $overlay.hide();
+    });
+
+    // Close the card when clicking outside it
+    $overlay.on("click", function () {
+        $card.hide();
+        $(this).hide();
     });
 });
